8b0619fe67617655fb73c64fe0b151e8756109f6
Lucas Paulino <lucaspaulino.slv@gmail.com>
2022-07-13 21:47:28 -0300

    Create Disable_Users.ps1

---
+++ Limit User Logon Hours/Disable_Users.ps1
@@ -0,0 +1,10 @@
+﻿$user = "Informe_o_email"
+$PWord = Get-Content "Caminho_do_arquivo_de_senha"| ConvertTo-SecureString
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $PWord
+
+connect-Msolservice -Credential $Credential
+
+Import-Csv Caminho_do_csv | ForEach {Set-MsolUser - UserPrincipalName $_.name -bockCredential $true}
+
+connect-azureAD -credential $Credential
+import-Csv "Caminho_do_csv" | % {Get-azureADUser -SearchString $_.name | Revoke-AzureADUserAllRefreshToken}
